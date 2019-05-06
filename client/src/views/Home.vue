c99ddb15859e0ee0f12252876410e95851e35bfe
Macbook <macbook@MacBook-Pro-Macbook-2.local>
2019-05-06 16:25:06 +0200

    Server added

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div class="jumbotron">
-      <h1 class="display-3">Welcome to the jungle! 💚</h1>
+      <h1 class="display-3">Welcome to the jungle!</h1>
       <!-- <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p> -->
       <hr class="my-4">
       <!-- <p>It uses utility classes for typography and spacing to space content out within the larger container.</p> -->
