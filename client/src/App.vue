4d0473db6f700e2ef9f7250a1ef21a0c09c530a3
Macbook <macbook@MacBook-Pro-Macbook-2.local>
2019-05-06 14:47:39 +0200

    Initial Commit

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <app-nav></app-nav>
+
+    <div id="nav">
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import AppNav from '@/components/AppNav';
+export default {
+  components:{
+    AppNav,
+  },
+};
+</script>
+
+
+<style lang="scss">
+</style>
